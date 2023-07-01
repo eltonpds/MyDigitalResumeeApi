76c1f6c43b82c03bd028a48ce668511efa393480
eltonpds <eltonpereira505@gmail.com>
2023-07-01 18:18:39 -0300

    Criado o método GetUsuarioPorId

--- MyDigitalResumeeApi/Controllers/UsuarioController.cs
+++ MyDigitalResumeeApi/Controllers/UsuarioController.cs
@@ -19,11 +19,21 @@ namespace MyDigitalResumeeApi.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Usuario>>> GetAllUsers()
+        public ActionResult<IEnumerable<Usuario>> GetAllUsers()
         {
-            IEnumerable<Usuario> usuarios = await _connection.QueryAsync<Usuario>("SELECT Nome, Email, Cpf, DataNascimento, Celular, Cep, Endereco, Bairro, Cidade, Estado, Pais");
+            IEnumerable<Usuario> usuarios = _connection.Query<Usuario>("SELECT Nome, Email, Cpf, DataNascimento, Celular, Cep, Endereco, Bairro, Cidade, Estado, Pais");
 
             return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Usuario> GetUsuarioPorId(int id)
+        {
+            Usuario usuario = _connection.QueryFirst<Usuario>("SELECT Nome, Email, Cpf, DataNascimento, Celular, Cep, Endereco, Bairro, Cidade, Estado, Pais FROM Usuario WHERE Id = @Id", new
+            {
+                @Id = id
+            });
+            return Ok(usuario);
         }
 
         /// <summary>
@@ -49,16 +59,23 @@ namespace MyDigitalResumeeApi.Controllers
         /// </remarks>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult InsertUser(Usuario usuario)
+        public ActionResult InserirUsuario(Usuario usuario)
         {
             _connection.Execute("INSERT INTO Usuario VALUES(@Nome, @Email, @Senha, @Cpf, @DataNascimento, @Celular, @Cep, @Endereco, @Bairro, @Cidade, @Estado, @Pais)", usuario);
             return Ok();
         }
 
         [HttpPut]
-        public ActionResult UpdateUser(Usuario usuario)
+        public ActionResult AtualizarUsuario(Usuario usuario)
         {
             _connection.Execute("UPDATE Usuario SET Nome = @Nome, Email = @Email, Senha = @Senha, Cpf = @Cpf, DataNascimento = @DataNascimento, Celular = @Celular, Cep = @Cep, Endereco = @Endereco, Bairro = @Bairro, Cidade = @Cidade, Estado = @Estado, Pais = @Pais WHERE Id = @Id", usuario);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult DeletarUsuario(int id)
+        {
+            _connection.Execute("DELETE FROM Usuario WHERE Id = @Id", new { @Id = id });
             return Ok();
         }
     }
